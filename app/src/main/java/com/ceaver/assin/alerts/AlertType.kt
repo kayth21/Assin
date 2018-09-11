612ded218e178e3b6eeed8d83cb687d1d2e5f812
Thomas <thomas@kayser.com>
2018-09-11 23:21:03 +0200

    Implementation of AlertWorker
    - "Recurring Stable" only
    - recursive call of check method until alert is set in range again

--- app/src/main/java/com/ceaver/assin/alerts/AlertType.kt
+++ app/src/main/java/com/ceaver/assin/alerts/AlertType.kt
@@ -1,5 +1,27 @@
 package com.ceaver.assin.alerts
 
+import java.util.*
+
 enum class AlertType {
-    ONE_TIME, RECURRING_STABLE, RECURRING_PERCENTAGE
+    ONE_TIME {
+        override fun check(alert: Alert, currentPrice: Double): Optional<Alert> {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    },
+    RECURRING_STABLE {
+        override fun check(alert: Alert, currentPrice: Double): Optional<Alert> {
+            return when {
+                currentPrice <= (alert.source - alert.target) -> Optional.of(alert.copy(source= alert.source - alert.target))
+                currentPrice >= (alert.source + alert.target) -> Optional.of(alert.copy(source= alert.source + alert.target))
+                else -> Optional.empty()
+            }
+        }
+    },
+    RECURRING_PERCENTAGE {
+        override fun check(alert: Alert, currentPrice: Double): Optional<Alert> {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    };
+
+    abstract fun check(alert: Alert, currentPrice: Double): Optional<Alert>
 }
--- app/src/main/java/com/ceaver/assin/alerts/AlertWorker.kt
+++ app/src/main/java/com/ceaver/assin/alerts/AlertWorker.kt
@@ -1,9 +1,18 @@
 package com.ceaver.assin.alerts
 
 import androidx.work.Worker
+import com.ceaver.assin.assets.Symbol
+import com.ceaver.assin.markets.MarketValuation
 
 class AlertWorker : Worker() {
     override fun doWork(): Result {
+        AlertRepository.loadAllAlerts().forEach { checkAlert(it) }
         return Result.SUCCESS
+    }
+
+    private fun checkAlert(alert: Alert) {
+        val currentPrice = MarketValuation.load(alert.symbol, Symbol.USD).get().last
+        val result = alert.alertType.check(alert, currentPrice)
+        result.ifPresent { AlertRepository.updateAlert(it); checkAlert(it) }
     }
 }
