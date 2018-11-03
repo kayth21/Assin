14665a6544262b8c5427cd4128c7a5879cb74b5c
Thomas <thomas@kayser.com>
2018-11-03 23:14:38 +0100

    alert reference field (ui only)

--- app/src/main/java/com/ceaver/assin/alerts/Alert.kt
+++ app/src/main/java/com/ceaver/assin/alerts/Alert.kt
@@ -9,6 +9,7 @@ import com.ceaver.assin.assets.Symbol
 data class Alert(//
         @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
         @ColumnInfo(name = "symbol") var symbol: Symbol = Symbol.BTC,
+        @ColumnInfo(name = "reference") var reference: Symbol = Symbol.USD,
         @ColumnInfo(name = "alertType") var alertType: AlertType = AlertType.RECURRING_STABLE,
         @ColumnInfo(name = "source") var source: Double = 0.0,
         @ColumnInfo(name = "target") var target: Double = 0.0,
