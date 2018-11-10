7e25190560cee1d04bac6e0321a75b6c24315942
Thomas <thomas@kayser.com>
2018-11-10 22:51:35 +0100

    remove cents in fiat, if zero

--- app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
+++ app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
@@ -5,7 +5,10 @@ import com.ceaver.assin.assets.Symbol
 fun Double.format(symbol: Symbol): String {
     return if (symbol.isCrypto()) {
         val s = "%.8f".format(this)
-        if (s.indexOf(".") < 0) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
+        if (!s.contains(".")) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
     }
-    else "%.2f".format(this)
+    else {
+        val s = "%.2f".format(this)
+        if (!s.contains(".")) s else if((s.indexOf(".") < s.length-2) && !s.endsWith("00")) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
+    }
 }
