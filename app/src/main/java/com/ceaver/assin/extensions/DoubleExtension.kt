55217378d664e72334ce8b1e597b94ca9d67c384
Thomas <thomas@kayser.com>
2018-11-10 22:37:20 +0100

    remove leading zeros in cryptos

--- app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
+++ app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
@@ -2,4 +2,10 @@ package com.ceaver.assin.extensions
 
 import com.ceaver.assin.assets.Symbol
 
-fun Double.format(symbol: Symbol) : String = if (symbol.isCrypto()) "%.8f".format(this) else "%.2f".format(this)
+fun Double.format(symbol: Symbol): String {
+    return if (symbol.isCrypto()) {
+        val s = "%.8f".format(this)
+        if (s.indexOf(".") < 0) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
+    }
+    else "%.2f".format(this)
+}
