3c55ddae7b19120ea349d93187ae2245fc17bba5
Thomas <thomas@kayser.com>
2018-11-11 01:06:01 +0100

    precision 3 for fiat value representation

--- app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
+++ app/src/main/java/com/ceaver/assin/extensions/DoubleExtension.kt
@@ -9,7 +9,7 @@ fun Double.format(symbol: Symbol): String {
         if (!s.contains(".")) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
     }
     else {
-        val s = "%.2f".format(this)
+        val s = "%.2f".format(this.toBigDecimal().round(MathContext(3)))
         if (!s.contains(".")) s else if((s.indexOf(".") < s.length-2) && !s.endsWith("00")) s else s.replace(Regex("0*$"), "").replace(Regex("\\.$"), "")
     }
 }
