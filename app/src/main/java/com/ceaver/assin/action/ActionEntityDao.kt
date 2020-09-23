4d565369278a7ae9815744fd3e9a2b7a23ae5591
Thomas <thomas@kayser.com>
2020-09-23 22:49:01 +0200

    - brackets doesn't work, use single quotes

--- app/src/main/java/com/ceaver/assin/action/ActionEntityDao.kt
+++ app/src/main/java/com/ceaver/assin/action/ActionEntityDao.kt
@@ -10,17 +10,17 @@ import com.ceaver.assin.database.BaseEntityDao
 interface ActionEntityDao : BaseEntityDao<ActionEntity> {
 
     @Transaction
-    @Query("select * from [action] where id = :id")
+    @Query("select * from 'action' where id = :id")
     suspend fun loadById(id: Long): ActionDto
 
     @Transaction
-    @Query("SELECT * FROM [action]")
+    @Query("SELECT * FROM 'action'")
     suspend fun loadAll(): List<ActionDto>
 
     @Transaction
-    @Query("select * from [action]")
+    @Query("select * from 'action'")
     fun loadAllObserved(): LiveData<List<ActionDto>>
 
-    @Query("delete from [action]")
+    @Query("delete from 'action'")
     suspend fun deleteAll()
 }
