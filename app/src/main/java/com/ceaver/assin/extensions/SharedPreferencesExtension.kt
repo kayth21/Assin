faad2714763a3cd10f827b12e649bca211e2e758
Thomas <thomas@kayser.com>
2020-10-04 18:45:17 +0200

    Fix gson issue

--- app/src/main/java/com/ceaver/assin/extensions/SharedPreferencesExtension.kt
+++ app/src/main/java/com/ceaver/assin/extensions/SharedPreferencesExtension.kt
@@ -1,6 +1,8 @@
 package com.ceaver.assin.extensions
 
 import android.content.SharedPreferences
+import com.ceaver.assin.markets.CryptoTitle
+import com.ceaver.assin.markets.FiatTitle
 import com.ceaver.assin.markets.Title
 import com.google.gson.Gson
 
@@ -8,9 +10,14 @@ fun SharedPreferences.getString(key: String): String {
     return if (contains(key)) getString(key, "")!! else throw IllegalStateException()
 }
 
-fun SharedPreferences.getTitle(key: String) : Title {
+fun SharedPreferences.getCryptoTitle(key: String) : Title {
     val json = getString(key)
-    return Gson().fromJson(json, Title::class.java)
+    return Gson().fromJson(json, CryptoTitle::class.java)
+}
+
+fun SharedPreferences.getFiatTitle(key: String) : Title {
+    val json = getString(key)
+    return Gson().fromJson(json, FiatTitle::class.java)
 }
 
 fun SharedPreferences.Editor.setTitle(key: String, title: Title): SharedPreferences.Editor {
--- app/src/main/java/com/ceaver/assin/preferences/Preferences.kt
+++ app/src/main/java/com/ceaver/assin/preferences/Preferences.kt
@@ -2,8 +2,9 @@ package com.ceaver.assin.preferences
 
 import android.preference.PreferenceManager
 import com.ceaver.assin.AssinApplication
+import com.ceaver.assin.extensions.getCryptoTitle
+import com.ceaver.assin.extensions.getFiatTitle
 import com.ceaver.assin.extensions.getString
-import com.ceaver.assin.extensions.getTitle
 
 object Preferences {
     const val CRYPTO_TITLE = "cryptoTitle"
@@ -15,7 +16,7 @@ object Preferences {
     fun getCryptoTitleSymbol() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getString(CRYPTO_TITLE_SYMBOL)
     fun getFiatTitleSymbol() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getString(FIAT_TITLE_SYMBOL)
 
-    fun getCryptoTitle() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getTitle(CRYPTO_TITLE)
-    fun getFiatTitle() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getTitle(FIAT_TITLE)
+    fun getCryptoTitle() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getCryptoTitle(CRYPTO_TITLE)
+    fun getFiatTitle() = PreferenceManager.getDefaultSharedPreferences(AssinApplication.appContext).getFiatTitle(FIAT_TITLE)
 }
 
