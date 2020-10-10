bb1ddcadc209c8bfd26ed5959ae30d872d914c1d
Thomas <thomas@kayser.com>
2020-10-10 13:13:34 +0200

    Set correct close-values when splitting closed positions

--- app/src/main/java/com/ceaver/assin/positions/PositionFactory.kt
+++ app/src/main/java/com/ceaver/assin/positions/PositionFactory.kt
@@ -41,15 +41,17 @@ object PositionFactory {
                             id = originalPosition.id.addZeroDotOneToLastDecimal(),
                             quantity = action.quantity,
                             openValueCrypto = originalPosition.openValueCrypto.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.quantity),
-                            openValueFiat = originalPosition.openValueFiat.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.quantity)
-                            // TOOD closeValueCorrection needed if closed positions can be splitted
+                            openValueFiat = originalPosition.openValueFiat.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.quantity),
+                            closeValueCrypto = if (originalPosition.closeDate == null) null else originalPosition.closeValueCrypto!!.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.quantity),
+                            closeValueFiat = if (originalPosition.closeDate == null) null else originalPosition.closeValueFiat!!.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.quantity)
                     )
                     val remainingPosition = originalPosition.copy(
                             id = originalPosition.id.addZeroDotTwoToLastDecimal(),
                             quantity = action.remaining,
                             openValueCrypto = originalPosition.openValueCrypto.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.remaining),
-                            openValueFiat = originalPosition.openValueFiat.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.remaining)
-                            // TOOD closeValueCorrection needed if closed positions can be splitted
+                            openValueFiat = originalPosition.openValueFiat.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.remaining),
+                            closeValueCrypto = if (originalPosition.closeDate == null) null else originalPosition.closeValueCrypto!!.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.remaining),
+                            closeValueFiat = if (originalPosition.closeDate == null) null else originalPosition.closeValueFiat!!.divide(originalPosition.quantity, MathContext.DECIMAL32).times(action.remaining)
                     )
                     positions.remove(originalPosition)
                     positions.add(splitPosition)
--- app/src/test/java/com/ceaver/assin/positions/PositionFactoryTest.kt
+++ app/src/test/java/com/ceaver/assin/positions/PositionFactoryTest.kt
@@ -76,6 +76,29 @@ class PositionFactoryTest {
     }
 
     @Test
+    @DisplayName("Split action on closed position lead in removing origin position and two new closed positions")
+    fun splitAction2() {
+        // arrange
+        val deposit = Deposit.fromTestdata(quantity = 10.toBigDecimal(), valueFiat = 1000.toBigDecimal(), valueCrypto = 100.toBigDecimal())
+        val withdraw = Withdraw.fromTestdata(quantity = deposit.quantity, title = deposit.title, label = deposit.label, positionId = BigDecimal.ONE, valueFiat = 1111.toBigDecimal(), valueCrypto = 111.toBigDecimal())
+        val split = Split.fromTestdata(quantity = 4.toBigDecimal(), remaining = 6.toBigDecimal(), title = deposit.title, label = deposit.label, positionId = BigDecimal.ONE)
+        // act
+        val positions = PositionFactory.fromActions(listOf(deposit, withdraw, split))
+        // assert
+        assertThat(positions).hasSize(2)
+        assertThatPosition(positions[0])
+                .hasId(1.1.toBigDecimal())
+                .hasPosition(4.toBigDecimal(), deposit.title, deposit.label)
+                .hasOpenValues(deposit.date, 400.toBigDecimal(), 40.toBigDecimal())
+                .hasCloseValues(withdraw.date, 444.4.toBigDecimal(), 44.4.toBigDecimal())
+        assertThatPosition(positions[1])
+                .hasId(1.2.toBigDecimal())
+                .hasPosition(6.toBigDecimal(), deposit.title, deposit.label)
+                .hasOpenValues(deposit.date, 600.toBigDecimal(), 60.toBigDecimal())
+                .hasCloseValues(withdraw.date, 666.6.toBigDecimal(), 66.6.toBigDecimal())
+    }
+
+    @Test
     @DisplayName("Trade action lead in closed origin position and new open position")
     fun tradeAction() {
         // arrange
