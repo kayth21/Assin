054670eea42df294bd634ce116c65e38bf97786f
Kayth <thomas@kayser.com>
2022-03-12 18:03:06 +0100

    update coinpaprika client

--- app/src/main/java/com/ceaver/assin/markets/MarketRepository.kt
+++ app/src/main/java/com/ceaver/assin/markets/MarketRepository.kt
@@ -5,33 +5,33 @@ import com.ceaver.assin.AssinApplication
 import com.ceaver.assin.extensions.setTitle
 import com.ceaver.assin.markets.overview.MarketOverview
 import com.ceaver.assin.preferences.Preferences
-import com.coinpaprika.apiclient.api.CoinpaprikaApi
+import com.coinpaprika.apiclient.COINPAPRIKA_BASE_URL
+import com.coinpaprika.apiclient.FiatsService
+import com.coinpaprika.apiclient.GlobalStatsService
+import com.coinpaprika.apiclient.TickersService
 import com.coinpaprika.apiclient.entity.QuoteEntity
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object MarketRepository {
-    fun loadMarketOverview(): MarketOverview {
-        lateinit var marketOverview: MarketOverview
-        CoinpaprikaApi()
-                .global()
-                .unsubscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .blockingSubscribe(
-                        { marketOverview = MarketOverview.fromMarket(it!!) },
-                        { error -> error.printStackTrace() }) // TODO
-        return marketOverview
+
+    suspend fun loadMarketOverview(): MarketOverview {
+        val globalStatsService = createCoinpaprikaService(GlobalStatsService::class.java)
+        return MarketOverview.fromMarket(globalStatsService.getGlobalStats())
     }
 
     suspend fun loadAllTitles(): Triple<List<CryptoTitle>, List<FiatTitle>, List<CustomTitle>> {
         // load remote titles
-        val cryptoItems = CoinpaprikaApi().tickers()
-        val fiatItems = CoinpaprikaApi().fiats().filter { it.symbol == "USD" } // TODO Support more fiats
+        val tickersService = createCoinpaprikaService(TickersService::class.java)
+        val fiatsService = createCoinpaprikaService(FiatsService::class.java)
+        val cryptoItems = tickersService.getTickers("USD")
+        val fiatItems = fiatsService.getFiats().filter { it.id == "usd-us-dollars" } // TODO Support more fiats
          val customItems = TitleRepository.loadAllCustomTitles()
 
         // find reference titles in remote titles
         val cryptoEntity = cryptoItems.single { it.id == Preferences.getCryptoTitleId() }.let { it.quotes!!.get("USD") }!!
-        val fiatEntity = fiatItems.single { it.id == Preferences.getFiatTitleId() }.let { QuoteEntity(price = 1.0, athDate = null, athPrice = 1.0, dailyVolume = 1.0, dailyVolumeDailyChange = 1.0, marketCap = 1.0, marketCapDailyChange = 1.0, percentChange1h = 0.0, percentChange1y = 0.0, percentChange7d = 0.0, percentChange12h = 0.0, percentChange24h = 0.0, percentChange30d = 0.0, percentFromPriceAth = 0.0, volumeAdjusted = 0.0, volumeReported = 0.0) }         // TODO support fiat other USD
+        val fiatEntity = fiatItems.single { it.id == Preferences.getFiatTitleId() }.let { QuoteEntity(price = 1.0, athDate = null, athPrice = 1.0, dailyVolume = 1.0, dailyVolumeDailyChange = 1.0, marketCap = 1.0, marketCapDailyChange = 1.0, percentChange1h = 0.0, percentChange1y = 0.0, percentChange7d = 0.0, percentChange12h = 0.0, percentChange24h = 0.0, percentChange30d = 0.0, percentFromPriceAth = 0.0) }         // TODO support fiat other USD
 
         // transform remote items to assin titles
         val localCryptoTitles = TitleRepository.loadAllCryptoTitles()
@@ -47,4 +47,10 @@ object MarketRepository {
 
         return Triple(cryptoTitles, fiatTitles, customTitles)
     }
+
+    private fun <T : Any?> createCoinpaprikaService(serviceClass: Class<T>) = Retrofit.Builder()
+        .baseUrl(COINPAPRIKA_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build().create(serviceClass)
 }
